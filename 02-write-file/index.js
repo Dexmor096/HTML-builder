a3826123dad146425d3b2b5cfc54b5eafb449472
Dexter096 <gr1nch2@yandex.ru>
2023-05-05 20:36:08 +0300

    feat: 1 task complete

---
+++ 02-write-file/index.js
@@ -0,0 +1,12 @@
+const { stdin, stdout } = process;
+const fs = require('fs');
+const path = require('path');
+
+
+// const writeStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+// const message = stdout.write('Привет, как делишки? напиши  что-нибудь:');
+// writeStream.on('data', () => {
+//   console.log(message);
+// });
+// message.on('data', chunk => output.write(chunk));
+
