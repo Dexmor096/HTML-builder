12e20437ca50b857f352651d9db59c3bbbc6ba2d
Dexter096 <gr1nch2@yandex.ru>
2023-05-06 10:55:27 +0300

    feat: 3 task complete

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+const { stdout } = process;
+const path = require('path');
+const fsPromise = require('fs/promises');
+
+
+const readDirectory = async () => {
+  const directory = fsPromise.readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true});
+  const filesArray = await directory;
+  for (const file of filesArray) {
+    if (!file.isDirectory()) {
+      const fileName = path.parse(file.name).name;
+      const extensionName = path.extname(file.name).slice(1);
+      const statFile = await fsPromise.stat(path.join(__dirname,'secret-folder',file.name));
+      const weightFile = statFile.size;
+      
+      stdout.write(`${fileName} - ${extensionName} - ${weightFile}\n`);
+    }
+  }
+  
+};
+readDirectory();
