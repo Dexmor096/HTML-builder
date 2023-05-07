1f626363d8faa870b9b4169f463a1c3f72dd52b8
Gladky Konstantin <gr1nch2@yandex.ru>
2023-05-07 16:19:46 +0300

    feat: task 4 complete

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,8 +1,26 @@
-const fs = require('fs');
-const {stdout} = process;
 const path = require('path');
+const { copyFile, mkdir, readdir, unlink, rm } = require('node:fs/promises');
 
-fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
-    if (err) throw err;
-})
-console.log('directory has created');
+
+const copy = async () => {
+    try {
+
+        await mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+
+        const fileList = await readdir(path.join(__dirname, 'files'));
+        const fileCopyList = await readdir(path.join(__dirname, 'files-copy'))
+        console.log(fileCopyList.length);
+
+        for (const file of fileCopyList) {
+            await unlink(path.join(__dirname, 'files-copy', file));
+        }
+        for (const file of fileList) {
+            await copyFile(
+                path.join(__dirname, 'files', file),
+                path.join(__dirname, 'files-copy', file));
+        }
+    } catch {
+        console.error('The file could not be copied');
+    }
+}
+copy();
